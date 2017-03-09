4e175c09579c323b5e679fc094ede4017dc13a5b
gplesz <gabor.plesz@gmail.com>
2017-03-09 11:12:51 +0100

    #1: Adapter minta, IDbDataAdapter - programozás felület ellen - TDD

---
+++ APDP201703/01Adapter.Tests/AddressDbDataAdapterRepositoryTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+using System.Data;
+
+namespace _01Adapter.Tests
+{
+
+    /// <summary>
+    /// 
+    /// 
+    ///    Repository ----()------> Adatok(adatbázis)
+    ///    ebből indirekcióval
+    ///    Repository ----> IDbDataAdapter ------> Adatbázis
+    /// 
+    /// </summary>
+
+
+
+    [TestClass]
+    public class AddressDbDataAdapterRepositoryTests
+    {
+        [TestMethod]
+        public void AddressDbDataAdapterRepositoryShouldThrowIfArgumentNull()
+        {
+            //Arrange
+            AddressDbDataAdapterRepository sut;
+
+            //Act
+            Action todo = () => sut = new AddressDbDataAdapterRepository(null);
+
+            //Assert
+            todo.ShouldThrow<ArgumentNullException>();
+
+        }
+    }
+}
---
+++ APDP201703/01Adapter/AddressDbDataAdapterRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace _01Adapter
+{
+    public class AddressDbDataAdapterRepository : IAddressRepository
+    {
+        private IDbDataAdapter adapter;
+
+        public AddressDbDataAdapterRepository(IDbDataAdapter adapter)
+        {
+            if (adapter == null) { throw new ArgumentNullException(nameof(adapter)); }
+            this.adapter = adapter;
+        }
+
+        public IList<Address> GetAddresses()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
