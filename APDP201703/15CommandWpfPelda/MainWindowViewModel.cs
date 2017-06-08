d790ce9f3c6225efe2d0a8ce5914d3d87b6eb32b
gplesz <gabor.plesz@gmail.com>
2017-06-08 16:01:12 +0200

    #10: Parancs példa WPF-ben: bevezetés  - működő kód parancs minta nélkül

---
+++ APDP201703/15CommandWpfPelda/MainWindowViewModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _15CommandWpfPelda
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+        private CancellationTokenSource cts = null;
+
+        private int progressValue = 0;
+        public int ProgressValue
+        {
+            get { return progressValue; }
+            set
+            {
+                if (value==ProgressValue) { return; }
+                progressValue = value;
+                OnPropertyChanged(nameof(ProgressValue));
+            }
+        }
+
+        public async void Start()
+        {
+            cts = new CancellationTokenSource();
+            var task = new Task(() => {
+                for (int i = 0; i < 100; i++)
+                {
+                    if (cts.Token.IsCancellationRequested)
+                    {
+
+                    }
+                    cts.Token.ThrowIfCancellationRequested();
+                    ProgressValue=i;
+                    Thread.Sleep(20);
+                }
+            },cts.Token);
+
+            task.Start();
+            try
+            {
+                await Task.WhenAll(task);
+            }
+            catch (OperationCanceledException)
+            { //ide akkor jövünk, ha a taskot megszakították
+            }
+
+        }
+
+        public void Stop()
+        {
+            if (cts==null) { return; }
+            cts.Cancel();
+        }
+    }
+}
---
+++ APDP201703/15CommandWpfPelda/info.md
@@ -0,0 +1,10 @@
+﻿# A WPF alkalmazásfejlesztés ajánlott módszere az MVVM
+- M mint Model
+- V mint View
+- VM min ViewModel
+
+## View: MainVindow.xaml
+## ViewModel: MainWindowViewModel.cs
+
+## DataBinding: ami a ViewModel adatain változik, azt a View automatikusan megjeleníti
+
